f31a7532e62501bbe568917540bb150d2ec8ac41
Nick Papadiochos <npapadiochos@singularlogic.eu>
2026-01-30 18:36:36 +0200

    chore: 🚧 Remove Samples

--- sample/App1/Configs/IdentityConfig.cs
+++
@@ -1,6 +0,0 @@
-namespace App1.Configs;
-
-internal sealed class IdentityConfig
-{
-    public required string Url { get; init; }
-}
--- sample/Yarp/Configs/IdentityConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace Yarp.Configs;
-
-internal sealed class IdentityConfig
-{
-    public required string Url { get; init; }
-    public required string ClientId { get; init; }
-    public required string ClientSecret { get; init; }
-}
